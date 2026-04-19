676e80a869b238bdd2b00d1d96fd47523b530dbb
Jenipher Alum <117021396+Jennyal30@users.noreply.github.com>
2026-04-19 11:35:14 +0300

    Update absorption.py
    Extend absorption model to multiple nutrients

--- models/absorption.py
+++ models/absorption.py
@@ -1,13 +1,15 @@
-# Absorption model for micronutrients
+# Multi-nutrient absorption model
 
-def absorbable_iron(food):
-    iron = food["iron"]
+def absorbable_nutrients(food):
     phytate = food["phytate"]
 
-    # simple anti-nutrient inhibition
-    absorption_factor = 1 - (0.25 * phytate)
+    # absorption penalty
+    factor = 1 - (0.25 * phytate)
+    if factor < 0:
+        factor = 0
 
-    if absorption_factor < 0:
-        absorption_factor = 0
-
-    return iron * absorption_factor
+    return {
+        "iron": food["iron"] * factor,
+        "zinc": food["zinc"] * factor,
+        "calcium": food["calcium"] * factor
+    }
